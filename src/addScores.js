55be7d208b6824ce7e8b19ce677ceb66d28b0133
jonas-45 <jonaswullah@gmail.com>
2023-02-07 16:14:42 +0000

    Add form to allow user to add scores

---
+++ src/addScores.js
@@ -0,0 +1,14 @@
+
+
+const content = document.getElementById('content');
+
+const addScores = `
+<div class="add-score">
+<h2>Add your score</h2>
+<form id="form">
+  <input type="text" class="input-txt" placeholder="Your name"><br>
+  <input type="text" class="input-txt" placeholder="Your score"><br>
+  <input type="submit" id="submit" value="Submit">
+</form>
+</div>
+`;
